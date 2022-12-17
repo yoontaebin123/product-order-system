cf9202f6b773f73dbda798753be94ae07686dad3
윤태빈 <gusckd000@gmail.com>
2022-12-17 23:25:25 +0900

    Add Getting Started with Component Scanning and Automated Dependency Injection

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    //@Bean orderService -> new MemoryMemberRepository()
-
-    //call AppConfig.memberService
-    //call AppConfig.memberRepository
-    //call AppConfig.memberRepository
-    //call AppConfig.orderService
-    //call AppConfig.memberRepository
-
-    //call AppConfig.memberService
-    //call AppConfig.memberRepository
-    //call AppConfig.orderService
-
     @Bean
     public MemberService memberService() {
         System.out.println("call AppConfig.memberService");
