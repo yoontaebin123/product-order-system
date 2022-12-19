d564e46da5c512e325e9c50ec2d7f9bb60dd05f7
윤태빈 <gusckd000@gmail.com>
2022-12-19 09:18:55 +0900

    Add More than 2 lookup beans - problem

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -30,8 +30,8 @@ public class AppConfig {
     @Bean
     public OrderService orderService() {
         System.out.println("call AppConfig.orderService");
-//        return new OrderServiceImpl(memberRepository(), discountPolicy());
-        return null;
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+//        return null;`
     }
 
     @Bean
